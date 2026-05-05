a57ea7e19f728e2174dc4d710c8435e2f2b32993
felixfsdev <faseeh1080@gmail.com>
2026-05-05 11:19:31 +0000

    add tailwind typography plugin

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,25 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-2 min-w-2xl mx-auto">
-      <h1 className="text-2xl">Hello world!</h1>
+    <div className="prose min-w-2xl mx-auto p-4">
+      <h1>Hello world!</h1>
       <p>This is a Next.js website.</p>
-      <div className="flex gap-2">
+      <div className="not-prose flex gap-2">
         <Button className="w-min">Click Me!</Button>
         <Button variant="outline">Me Too!</Button>
         <Button className="w-min" disabled>
-          Not Me
+          Not Me, I'm Disabled
         </Button>
       </div>
+      <h2>Heading 2</h2>
+      <p className="text-xs text-muted-foreground">
+        I am not that important :(
+      </p>
+      <p>
+        Next.js is a React framework that enables you to build static and
+        server-rendered applications. It provides features like automatic code
+        splitting, optimized performance, and a great developer experience.
+      </p>
     </div>
   );
 }
