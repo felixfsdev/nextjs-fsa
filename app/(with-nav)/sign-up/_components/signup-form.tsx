c7a3b95b74bbddd18eeca19bddef6ec0701e501f
felixfsdev <faseeh1080@gmail.com>
2026-05-09 11:31:14 +0000

    add signup (not working)

--- app/(with-nav)/sign-up/_components/signup-form.tsx
+++ app/(with-nav)/sign-up/_components/signup-form.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { authClient } from "@/lib/auth-client";
+
 export default function SignUpForm() {
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -10,7 +12,26 @@ export default function SignUpForm() {
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
 
-    alert(`Name: ${name}, Email: ${email}, Password: ${password}`);
+    const { data, error } = await authClient.signUp.email(
+      {
+        email, // user email address
+        password, // user password -> min 8 characters by default
+        name, // user display name
+        callbackURL: "/dashboard", // A URL to redirect to after the user verifies their email (optional)
+      },
+      {
+        onRequest: (ctx) => {
+          //show loading
+        },
+        onSuccess: (ctx) => {
+          //redirect to the dashboard or sign in page
+        },
+        onError: (ctx) => {
+          // display the error message
+          alert(ctx.error.message);
+        },
+      },
+    );
   }
 
   return (
