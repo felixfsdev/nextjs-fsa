20ac4ac66d46baeec76c264a979eb9f20b0663ff
felixfsdev <faseeh1080@gmail.com>
2026-05-09 12:44:17 +0000

    make sign up form actually sign up

--- app/(with-nav)/sign-up/sign-up-form.tsx
+++ app/(with-nav)/sign-up/sign-up-form.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 import Link from "next/link";
 import React from "react";
 
@@ -9,8 +10,43 @@ export default function SignUpForm() {
   async function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    const data = Object.fromEntries(formData);
-    alert(JSON.stringify(data, null, 2));
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const name = formData.get("name");
+
+    if (!email || !password || !name) {
+      return;
+    }
+
+    if (
+      typeof email !== "string" ||
+      typeof password !== "string" ||
+      typeof name !== "string"
+    ) {
+      return;
+    }
+
+    // Sign up the user
+    const { data, error } = await authClient.signUp.email(
+      {
+        email, // user email address
+        password, // user password -> min 8 characters by default
+        name, // user display name
+        callbackURL: "/dashboard", // A URL to redirect to after the user verifies their email (optional)
+      },
+      {
+        onRequest: (ctx) => {
+          //show loading
+        },
+        onSuccess: (ctx) => {
+          //redirect to the dashboard or sign in page
+        },
+        onError: (ctx) => {
+          // display the error message
+          alert(ctx.error.message);
+        },
+      },
+    );
   }
 
   return (
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,14 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/lib/db"; // your drizzle instance
+import * as schema from "@/auth-schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema: schema,
   }),
+
   emailAndPassword: {
     enabled: true,
   },
