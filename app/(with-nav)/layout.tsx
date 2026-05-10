67eb603c841bd208ab1e3326f4f040efdcf26ba8
felixfsdev <faseeh1080@gmail.com>
2026-05-10 08:19:27 +0000

    hide sign up when logged in

--- app/(with-nav)/layout.tsx
+++ app/(with-nav)/layout.tsx
@@ -1,12 +1,18 @@
 import Navbar from "@/components/layout/navbar";
-import { SiGithub } from "react-icons/si";
 import { Button } from "@/components/ui/button";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import Link from "next/link";
 
-export default function WithNavLayout({
+export default async function WithNavLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar
@@ -20,9 +26,14 @@ export default function WithNavLayout({
               { label: "Sub Item 2", path: "/fake/2" },
             ],
           },
-          { type: "link", label: "Sign Up", path: "/sign-up" },
         ]}
-      />
+      >
+        {session === null && (
+          <Link href="/sign-up">
+            <Button>Sign Up</Button>
+          </Link>
+        )}
+      </Navbar>
       <main className="flex-1">{children}</main>
     </div>
   );
