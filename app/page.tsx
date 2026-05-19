9f897ee2b62829b25b66b71dec3b61df70b9d3a1
felixfsdev <faseeh1080@gmail.com>
2026-05-19 11:19:54 +0000

    style: update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import { Button } from "@/components/ui/button";
 
 export default function LandingPage() {
   return (
-    <div className="w-full max-w-md mx-auto h-dvh flex items-center text-center justify-center gap-8">
+    <div className="w-full flex-col sm:flex-row max-w-md mx-auto h-dvh flex items-center text-center justify-center gap-8">
       <h1 className="text-6xl">fsa</h1>
       <div className="flex flex-col gap-4 items-center justify-center">
         <p>
           Easy and fast chatrooms
           <br />
-          Privacy <i>NOT</i> guaranteed
+          Privacy <i>not</i> guaranteed
         </p>
         <Button className="animate-pulse">Let's Chat</Button>
       </div>
