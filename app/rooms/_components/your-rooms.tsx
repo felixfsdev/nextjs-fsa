a410049c3087a5d5b6e2cf5160c4c54ca84f9105
felixfsdev <faseeh1080@gmail.com>
2026-05-20 15:20:49 +0000

    create create room form

--- app/rooms/_components/your-rooms.tsx
+++ app/rooms/_components/your-rooms.tsx
@@ -2,6 +2,18 @@ import { Button } from "@/components/ui/button";
 import { db } from "@/db";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default async function YourRooms() {
   const session = await auth.api.getSession({
@@ -31,9 +43,41 @@ export default async function YourRooms() {
         </ul>
       )}
 
-      <Button variant={rooms.length === 0 ? "default" : "outline"}>
-        Create Room
-      </Button>
+      <Dialog>
+        <form>
+          <DialogTrigger asChild>
+            <Button
+              variant={rooms.length === 0 ? "default" : "outline"}
+              className="w-full"
+            >
+              Create Room
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-sm text-foreground">
+            <DialogHeader>
+              <DialogTitle>Create Room</DialogTitle>
+              <DialogDescription>
+                Invite your friends with a secure link. No sign in required for
+                them.
+              </DialogDescription>
+            </DialogHeader>
+            <Label htmlFor="username">Name</Label>
+            <Input
+              id="username"
+              name="username"
+              placeholder="The name of the chatroom"
+            />
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogClose>
+              <Button type="submit" className="w-28">
+                Create
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </form>
+      </Dialog>
     </div>
   );
 }
